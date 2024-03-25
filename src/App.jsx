49bf0c9ea75250249124c3c4f894302258d1680f
Jahid458 <jahidul898@outlook.com>
2024-03-26 02:15:14 +0600

    Enable Multi Selection

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Accordian from './components/accordian'
+
+function App() {
+
+  return (
+    <>
+    {/* Accordian Component  */}
+    <Accordian />
+     
+    </>
+  )
+}
+
+export default App
